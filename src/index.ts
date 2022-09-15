48d83f8aadebdedb34c1f5480612eee8d5a16fac
Óscar de la Mata <oscar@hanzo.es>
2022-09-15 23:15:41 +0200

    fmt files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Declaration from "https://deno.land/x/postcss@8.4.16/lib/declaration.d.ts"
+import Declaration from "https://deno.land/x/postcss@8.4.16/lib/declaration.d.ts";
 
 export type unitType = {
-  from: string,
+  from: string;
   convert: (val: number) => string;
 };
 
@@ -9,39 +9,40 @@ export interface optsType {
   units: unitType[];
 }
 
-const plugin = ( opts?:optsType )  => {
-  let myUnits:unitType[] = [];
+const plugin = (opts?: optsType) => {
+  let myUnits: unitType[] = [];
 
   if (!opts || !opts.units) {
     myUnits.push(
       {
-        from: 'sp',
-        convert: val => `${val * 0.25}rem`
-      }
+        from: "sp",
+        convert: (val) => `${val * 0.25}rem`,
+      },
     );
   } else {
     myUnits = [...opts.units];
   }
 
   return {
-    postcssPlugin: 'postcss-convert-units',
+    postcssPlugin: "postcss-convert-units",
 
     Declaration: {
-      '*': (decl: Declaration) => {
+      "*": (decl: Declaration) => {
         myUnits.forEach(
           (unit) => {
-            const regex = new RegExp('(([\\d.]+)' + unit.from + ')', 'g');
+            const regex = new RegExp("(([\\d.]+)" + unit.from + ")", "g");
 
             decl.value = decl.value.replace(
               regex,
-              (_match:string, _p1:string, p2:string) => unit.convert(parseInt(p2)),
+              (_match: string, _p1: string, p2: string) =>
+                unit.convert(parseInt(p2)),
             );
-          }
+          },
         );
-      }
+      },
     },
-  }
-}
+  };
+};
 plugin.postcss = true;
 
 export default plugin;
--- src/spec/index.spec.ts
+++ src/spec/index.spec.ts
@@ -1,45 +1,47 @@
 import { assertEquals } from "https://deno.land/std@0.155.0/testing/asserts.ts";
 import postcss from "https://deno.land/x/postcss@8.4.16/mod.js";
 
-import plugin, {optsType} from '../index.ts';
+import plugin, { optsType } from "../index.ts";
 
-async function run (
+async function run(
   input: string,
   output: string,
-  opts?: optsType ) {
-
-  const result = await postcss([plugin(opts)]).process(input, { from: undefined })
+  opts?: optsType,
+) {
+  const result = await postcss([plugin(opts)]).process(input, {
+    from: undefined,
+  });
 
   assertEquals(result.css, output);
   assertEquals(result.warnings().length, 0);
 }
 
-Deno.test('converts sp to rem by default', async () => {
+Deno.test("converts sp to rem by default", async () => {
   await run(
-    'a{ margin: 1sp;}',
-    'a{ margin: 0.25rem;}'
+    "a{ margin: 1sp;}",
+    "a{ margin: 0.25rem;}",
   );
-})
+});
 
-Deno.test('uses custom convert function and units', async () => {
+Deno.test("uses custom convert function and units", async () => {
   await run(
-    'a{ margin: 1pw;}',
-    'a{ margin: 0.25rem;}',
+    "a{ margin: 1pw;}",
+    "a{ margin: 0.25rem;}",
     {
-      units : [{ from: 'pw', convert: (val:number) => `${val * 0.25}rem` }]
-    }
+      units: [{ from: "pw", convert: (val: number) => `${val * 0.25}rem` }],
+    },
   );
-})
+});
 
-Deno.test('uses multiple custom convert function and units', async () => {
+Deno.test("uses multiple custom convert function and units", async () => {
   await run(
-    'a{ margin: 1pw 1sp;}',
-    'a{ margin: 0.25rem 1rem;}',
+    "a{ margin: 1pw 1sp;}",
+    "a{ margin: 0.25rem 1rem;}",
     {
-      units : [
-        { from: 'pw', convert: (val:number):string => `${val * 0.25}rem` },
-        { from: 'sp', convert: (val:number):string => `${val}rem` }
-      ]
-    }
+      units: [
+        { from: "pw", convert: (val: number): string => `${val * 0.25}rem` },
+        { from: "sp", convert: (val: number): string => `${val}rem` },
+      ],
+    },
   );
-})
+});
